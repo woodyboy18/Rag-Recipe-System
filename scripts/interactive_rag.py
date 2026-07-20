dddf03820d2b7388236fbe0a78e4e14a0c0ce075
Your Name <you@example.com>
2026-07-20 19:35:10 +0530

    now

--- scripts/interactive_rag.py
+++ scripts/interactive_rag.py
@@ -12,7 +12,7 @@ import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
-from query.query_rewriter import rewrite_query
+from query.query_rewritter import rewrite_query
 
 from memory.conversation_memory import save_turn
 from memory.memory_retriever import retrieve_memory
@@ -160,6 +160,8 @@ def interactive_chat():
         print("\nRewritten Query:", rewritten_query)
 
         docs, indices = retrieve(rewritten_query)
+        
+        save_retrieval_cache(rewritten_query, docs)
 
         print("\nRetrieved Recipes:")
 
@@ -204,12 +206,13 @@ Instructions:
 
 1. Always use ONLY the Recipe Context.
 2. Do NOT invent ingredients or cooking steps.
-3. If the current query refers to a previous recipe (for example "I don't have eggs", "make it vegetarian", "give another option"), use the Previous Conversation to understand the user's intent.
-4. Respect all extracted constraints such as meal type, diet, cooking time and ingredient restrictions.
-5. If no suitable recipe exists in the Recipe Context, reply exactly:
+3. Do not generate multiple recipes with same title.
+4. If the current query refers to a previous recipe (for example "I don't have eggs", "make it vegetarian", "give another option"), use the Previous Conversation to understand the user's intent.
+5. Respect all extracted constraints such as meal type, diet, cooking time and ingredient restrictions.
+6. If no suitable recipe exists in the Recipe Context, reply exactly:
 Recipe not found in database.
 
-Return ONLY in the following format.
+Return ONLY one complete recipe in the following format.
 
 Title: <recipe title>
 
